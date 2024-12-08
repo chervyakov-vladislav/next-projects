b3f12f2f8fe0d9189f282a1d49f37dece034e340
Vladislav Chervyakov <chervyakovve@mail.ru>
2024-12-08 14:04:46 +0700

    feat: sentry setup

--- repair-shop-app/src/app/(rs)/error.tsx
+++ repair-shop-app/src/app/(rs)/error.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-// import * as Sentry from "@sentry/nextjs"
+import * as Sentry from "@sentry/nextjs"
 import { useEffect } from 'react'
 
 export default function Error({
@@ -11,9 +11,8 @@ export default function Error({
   reset: () => void
 }) {
   useEffect(() => {
-    // Log the error to an error reporting service
     console.error(error)
-    // Sentry.captureException(error)
+    Sentry.captureException(error)
   }, [error])
 
   return (
@@ -21,7 +20,6 @@ export default function Error({
       <h2>Something went wrong!</h2>
       <button
         onClick={
-          // Attempt to recover by trying to re-render the segment
           () => reset()
         }
       >
